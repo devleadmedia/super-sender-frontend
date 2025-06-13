9c27093ee5636b204058a3a4c179ea4015013f2b
Samuel <samuelcs131@gmail.com>
2025-06-13 07:23:32 -0300

    feat: adicionar componente LoadingFull com suporte a múltiplos loaders

---
+++ src/components/loading/LoadingFull.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    v-if="loaderStatusMultiple(loaderIds)"
+    class="fullscreen flex flex-center z-max bg-black-1 loading-full-screen"
+  >
+    <div class="q-loading__box column items-center">
+      <q-spinner color="white" size="5em" />
+      <div class="q-loading__message">
+        {{ text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useLoader } from '../../composables/useLoader'
+
+interface IProps {
+  text: string
+  loaderIds: string[]
+}
+
+defineProps<IProps>()
+
+const { loaderStatusMultiple } = useLoader()
+</script>
+
+<style lang="scss" scoped>
+.loading-full-screen {
+  background-color: rgba($dark, 0.4);
+}
+</style>
